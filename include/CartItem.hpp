014b0d5eba3e56521f594da261efe46ebaa7430a
mahyar-mahmudi-cod <mahyar.mhd.cod@gmail.com>
2025-04-07 14:13:55 +0330

    CartItem.hpp

---
+++ include/CartItem.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <include/Product.hpp>
+
+class CartItem
+{
+private:
+    Product product;
+    int quantity;
+
+public:
+    CartItem(const Product &product, int quantity);
+
+    void increaseQuantity(int qty);
+    int getTotalPrice() const;
+    void display() const;
+
+    std::string getProductName() const;
+    std::string getProductCategory() const;
+};
